d8212833406326450105214706b0313607d73645
Berry  Semexan <bsemexan@gmail.com>
2018-05-05 18:39:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+  //insert comment into "comments" div in this format:
+  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+ 
+  //create template string
+  var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  //create template function
+  var templateFn = _.template(commentTemplate);
+}
